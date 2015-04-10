c9129932602d7a000fbb0260466d9630546801da
wolffzheng <wolff_zheng@hotmail.com>
2015-04-10 15:52:37 +1000

    add books on 2015年 04月 10日 星期五 15:52:36 EST

--- manual_update.sh
+++ manual_update.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+temp_date=$(date)
 git branch tmp
 git checkout tmp
 git add . --all
 #echo "please input commit message:"
 #read message
-git commit -m "add books"
+git commit -m "add books on $temp_date"
 git checkout master
 git merge tmp
 git push origin master
